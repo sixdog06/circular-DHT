7e24d6a823d406b5b3593b4b4bb64a26044851dd
huanruiz <zhanghrharry@qq.com>
2019-04-11 00:47:22 +1000

    ack

--- cdht.py
+++ cdht.py
@@ -9,17 +9,25 @@ import random
 import numpy as np
 import re
 
-# send packet
-def snd(file_clientSocket, receiver_id, num_of_bytes, ack_number, filedata):
-	sender_message = ' '.join(('snd', str(sequence_number), str(num_of_bytes), str(ack_number)))
+# send packets of file (UDP with ack)
+def snd(file_clientSocket, receiver_id, event, sequence_number, ack_num, num_of_bytes, filedata):
+	sender_message = ' '.join((event, str(sequence_number), str(ack_num), str(num_of_bytes)))
 	sender_message = b'\r\n\r\n'.join((sender_message.encode(), filedata))
-	file_clientSocket.sendto(sender_message, ('127.0.0.1', 50000 + int(receiver_id)))
+	file_clientSocket.sendto(sender_message, ('127.0.0.1', 50000 + receiver_id))
 
-# send ack
-def ack(receiver_id, num_of_bytes, ack_number):
-	ackSocket = socket(AF_INET, SOCK_DGRAM)
-	message = ' '.join(('ack', str(num_of_bytes + ack_number)))
-	ackSocket.sendto(message.encode(), ('127.0.0.1', 50000 + receiver_id))
+def write_responding_log(event, run_time, sequence_number, data_size, ack_num):
+	responding_log = open('responding_log.txt', 'r+')
+	responding_log.read()
+	log = ' '.join((event, str(run_time), str(sequence_number), str(data_size), str(ack_num), '\n'))
+	responding_log.write(log) 
+	responding_log.close()
+
+def write_requesting_log(event, run_time, sequence_number, data_size, ack_num):
+	requesting_log = open('requesting_log.txt', 'r+')
+	requesting_log.read()
+	log = ' '.join((event, str(run_time), str(sequence_number), str(data_size), str(ack_num), '\n'))
+	requesting_log.write(log) 
+	requesting_log.close()
 
 # receive the ping command (UDP)
 def receive_ping_request():
@@ -33,19 +41,23 @@ def receive_ping_request():
 				f = open('received_file.pdf', 'rb+')
 				f.read() #move the cursor to the end
 				file_content = re.findall(b'.*\r\n\r\n([\d\D]*)', predecessor_id)[0]
-				#print(file_content)
 				f.write(file_content)
 				f.close()
-				serverSocket.sendto(b'ack', addr)
-				#ack(1, 300, 1)
+				ack_num = int(predecessor_id.split()[1].decode()) + int(predecessor_id.split()[3].decode())
+				ack_message = ' '.join(('ack', predecessor_id.split()[1].decode(), str(ack_num)))
+				serverSocket.sendto(ack_message.encode(), addr)
+				if predecessor_id.split()[0].decode() == 'fin_snd':
+					print('The file is received.')
+
 		except UnicodeDecodeError: # predecessor_id is the chunk of file
 			f = open('received_file.pdf', 'wb+')
 			file_content = re.findall(b'.*\r\n\r\n([\d\D]*)', predecessor_id)[0]
-			#print(file_content)
 			f.write(file_content)
 			f.close()
-			serverSocket.sendto(b'ack', addr)
-			#ack(1, 300, 1)
+
+			ack_num = int(predecessor_id.split()[1].decode()) + int(predecessor_id.split()[3].decode())
+			ack_message = ' '.join(('ack', predecessor_id.split()[1].decode(), str(ack_num)))
+			serverSocket.sendto(ack_message.encode(), addr)
 
 # receive the ping response (UDP)
 def receive_ping_response():
@@ -77,11 +89,12 @@ def request_file():
 
 # TCP server to get the file request
 def TCP_server():
+	global sequence_number
 	while True:
 		try:
 			connectionSocket, addr = TCP_serverSocket.accept()
 			data = connectionSocket.recv(1024)		
-			if data:
+			if data and not data.decode().split()[2] == 'start':
 				predecessor_id = int(data.decode().split()[1])
 				hash_value = int(data.decode().split()[2]) % 256 # hash function
 
@@ -89,34 +102,59 @@ def TCP_server():
 					print(f'File {data.decode().split()[2]} is here.')
 					print(f'A response message, destined for peer {int(data.decode().split()[0])}, has been sent.')
 					print('We now start sending the file ………')
+					
+					TCP_clientSocket = socket(AF_INET, SOCK_STREAM)
+					TCP_clientSocket.connect(('127.0.0.1', 50000 + int(data.decode().split()[0])))
+					reply = ' '.join((str(own_id), str(data.decode().split()[2]), 'start'))
+					TCP_clientSocket.send(reply.encode())
+					TCP_clientSocket.close()
+
+					
 					responding_log = open('responding_log.txt', 'w+') # create a file called responding_log.txt
 					responding_log.close()
-
+					f = open(data.decode().split()[2] + '.pdf', 'rb')
+					filesize = len(f.read())
+					f.close()
+					
 					# transfer the file
 					try:
 						with open(data.decode().split()[2] + '.pdf', 'rb') as f:
 							file_clientSocket = socket(AF_INET, SOCK_DGRAM)
 							file_clientSocket.settimeout(1) # timeout = 1s
 							filedata = f.read(MSS)
+							resend_flag = 0
+
 							while filedata:
-								#while True:
-								#snd(file_clientSocket, int(data.decode().split()[0]), len(filedata), 0, filedata)
-								sender_message = ' '.join(('own_id', 'snd', str(1), str(len(filedata)), str(1)))
-								sender_message = b'\r\n\r\n'.join((sender_message.encode(), filedata))
-								file_clientSocket.sendto(sender_message, ('127.0.0.1', 50000 + int(data.decode().split()[0])))
-								ack_message, (address, _) = file_clientSocket.recvfrom(1024)
-									#print('the data is', data)
-									#if data:
-									#	if str(data).split[0] == 'ack':
-									#		break
-									#else:
-									#	pass
+								while True:
+									if f.tell() >= filesize: # whether is the last send, use ack_num to send sequence number
+										snd(file_clientSocket, int(data.decode().split()[0]), 'fin_snd', sequence_number, 0, len(filedata), filedata)
+									else:
+										snd(file_clientSocket, int(data.decode().split()[0]), 'snd', sequence_number, 0, len(filedata), filedata)
+									
+									if resend_flag == 0:
+										# write snd log
+										write_responding_log('snd', round(time.time()-start_time, 2), sequence_number, len(filedata), 0)
+									else:
+										# write RTX log
+										write_responding_log('RTX', round(time.time()-start_time, 2), sequence_number, len(filedata), 0)
+										resend_flag = 0
+
+									ack_message, (address, _) = file_clientSocket.recvfrom(1024)
+									if ack_message:
+										if ack_message.split()[0].decode() == 'ack':
+											sequence_number = ack_message.split()[2].decode() # update sequence number
+											write_responding_log('rcv', round(time.time()-start_time, 2), 0, len(filedata), sequence_number)
+											break
+									else:
+										# write Drop log	
+										write_responding_log('DROP', round(time.time()-start_time, 2), 0, len(filedata), 0)
+										resend_flag = 1
 								filedata = f.read(MSS)
 							print('The file is sent.')
-							file_clientSocket.close()
+							file_clientSocket.close()				
+
 					except IOError:
 						pass
-
 				else:
 					print(f'File {data.decode().split()[2]} is not stored here.')
 					TCP_clientSocket = socket(AF_INET, SOCK_STREAM)
@@ -125,6 +163,9 @@ def TCP_server():
 					TCP_clientSocket.send(data)
 					TCP_clientSocket.close()
 					print('File request message has been forwarded to my successor.')	
+			elif data and data.decode().split()[2] == 'start':
+				print('We now start receiving the file ………')
+				print(f'Received a response message from peer {data.decode().split()[0]}, which has the file {data.decode().split()[1]}.')
 		except IOError:
 			pass
 
